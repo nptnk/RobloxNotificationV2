d2b2098fa93747554df19067f42e4072e249e1f4
nptnk <74488094+nptnk@users.noreply.github.com>
2022-08-07 12:58:01 +1000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -6,16 +6,29 @@ local gui = Instance.new("ScreenGui")
 gui.Parent = playergui
 gui.ResetOnSpawn = false
 gui.Name = "NotificationV2"
+gui.IgnoreGuiInset = true
+
+local notificationHolder = Instance.new("Frame")
+notificationHolder.Parent = gui
+notificationHolder.Name = "Frame"
+notificationHolder.AnchorPoint = Vector2.new(1,1)
+notificationHolder.Position = UDim2.new(1,0,1,0)
+notificationHolder.Size = UDim2.new(0.125,0,1,0)
+notificationHolder.BackgroundTransparency = 1
 
 local module = {}
 
-function module:new()
+function module:new(t)
     local mainFrame = Instance.new("Frame")
-    mainFrame.Parent = gui
+    mainFrame.Parent = notificationHolder
     mainFrame.Name = "Frame"
-    mainFrame.AnchorPoint = UDim.new(1,1)
+    mainFrame.AnchorPoint = Vector2.new(1,1)
     mainFrame.Position = UDim2.new(1,0,1,0)
-    mainFrame.Size = UDim2.new(0,200,0,45)
+    mainFrame.Size = UDim2.new(0,0,0.05,0)
+    mainFrame.BackgroundColor = Color3.fromRGB(33,33,33)
+    mainFrame.BorderSizePixel = 0
+    
+    ts:Create(mainFrame,TweenInfo.new(0.3,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),{Size = UDim2.new(1,0,0.05,0)}):Play()
 end
 
 return module
